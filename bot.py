3313d672d2787b83fea3a715631e5ab4c32690f7
Yulia199716 <yuliapetina@gmail.com>
2026-01-28 18:35:49 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8567616489:AAEX1OYjLKJ_k33jIFGgLbSqsDdD0-FxmQE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+    [InlineKeyboardButton("Календарь", url="https://clck.ru/3MscXu")],
+    [InlineKeyboardButton("Добавить мероприятие", url="https://clck.ru/3MrvFT")],
+    [InlineKeyboardButton("Заявка на вход", url="https://forms.yandex.ru/cloud/697743ab068ff06061e8a02e")],
+    [InlineKeyboardButton("Заявка на ВКС", url="https://forms.yandex.ru/cloud/65cc7cb92530c22a292928c9/?page=1")],
+    [InlineKeyboardButton("Телефонный справочник", url="https://sks-bot.ru/employee")]
+]
+
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Добро пожаловать!",
+        reply_markup=reply_markup
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен...")
+app.run_polling()
